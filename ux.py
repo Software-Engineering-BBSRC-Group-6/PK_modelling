47fd0bffbbd001c0b25d3af59bca714bf2039b64
Amit-H <aseenanden@gmail.com>
2021-10-21 12:10:14 +0100

    Allows the user to specify dosing protocol

--- ux.py
+++ ux.py
@@ -24,7 +24,24 @@ def user_input():
     print("Hello! This script builds a pharmacokinetic model.")
     model_type = input("What kind of models do you want to build? (intravenous bolous (ib) / subcutaneous (sc))")
     compound = input("What compound or drug are you using?")
-    dose =  input("What is the dose of " + compound + " that you want to test? (units in ng per hour)")
+    dose_type = input("How is the dose delivered? Constantly over time (c), Instantaneously (i) or Repeated instantaneous doses (r)")
+
+    if dose_type == 'c':
+        dose =  input("What is the dose of " + compound + " that you want to test? (units in ng per hour)")
+        dose_mass = None
+        time_dose = None
+        num_dose = None
+    elif dose_type == 'i':
+        dose_mass =  input("What is the mass of the dose of " + compound + " that you want to test? (units in ng)")
+        dose = None
+        time_dose = None
+        num_dose = None
+    elif dose_type == 'r':
+        dose_mass =  input("What is the mass of the dose of " + compound + " that you want to test? (units in ng)")
+        time_dose = input("What time period are the doses given over? (units in hours")
+        num_dose = input("How many doses are given? - this program assumes that doses are evenly spaced throughout the time period.")
+        dose = None
+    
     len_assay = input("Assay time? (units in hours)")
     len_interval = input("What interval time would you like? (units in hours)")
     clearance = input("What is the clearance time? (units in hours)")
@@ -84,7 +101,11 @@ def user_input():
     return {
         'model_type': model_type, 
         'compound': compound, 
-        'dose':dose, 
+        'dose_type': dose_type,
+        'dose':dose,
+        'dose_mass': dose_mass,
+        'time_dose': time_dose,
+        'num_dose': num_dose,
         'len_assay':len_assay, 
         'len_interval':len_interval, 
         'clearance':clearance, 
