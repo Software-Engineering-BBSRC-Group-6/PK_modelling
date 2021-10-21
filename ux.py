63100d7da45b638a6817d1b43f340c29a1b1dade
Amit-H <aseenanden@gmail.com>
2021-10-21 10:16:35 +0100

    UX file that generates a JSON file

--- ux.py
+++ ux.py
@@ -9,7 +9,6 @@ def user_input():
     This requests user input via the command line, and then returns the requrested arguments in a dictionary
 
     Variables:
-    len_model_type: How many models should be created? This should be an interger between 1 and 3
     model_type: Decides which model is build. This should return either intravenous bolous, subcutaenous or both
     compound: Allows the user to specify what the compound is. Useful for reporting back and for ease of understanding
     dose: Dose of compound used in ng per hour
@@ -22,36 +21,68 @@ def user_input():
 
     """
     print("Hello! This script builds a pharmacokinetic model.")
-    len_model_type = input("How many models do you want to build?")
-    model_type = input("What kind of models do you want to build? (intravenous bolous / subcutaenous / both)")
+    model_type = input("What kind of models do you want to build? (intravenous bolous (ib) / subcutaneous (sc))")
     compound = input("What compound or drug are you using?")
     dose =  input("What is the dose of " + compound + " that you want to test? (units in ng per hour)")
     len_assay = input("Assay time? (units in hours)")
     len_interval = input("What interval time would you like? (units in hours)")
     clearance = input("What is the clearance time? (units in hours)")
-    len_compartments = input("How many compartments do you want to test?")
-    len_compartments = int(len_compartments)
-
+    
     compartments = []
+    
+    if model_type == "ib":
 
-    for i in range(len_compartments):
-        compart = input("Enter volume (L), transition rate (), name of compartment and type of compartment (all seperated by spaces - eg: 5 25 upper-lung main)")
-        compart_list = compart.split()
-        compartments.append(compart_list)
+        main_compart = input("Enter volume (L), transition rate () for the main compartment (all seperated by spaces - eg: 5 25 )")
+        main_compart_split = main_compart.split()
+        main_compart_split.append(str("Main"))
+        compartments.append(main_compart_split)
+
+        num_peripherals = input("How many peripheral compartments do you want to test?")
+        
+        num_peripherals = int(num_peripherals)
+
+        if num_peripherals > 0:
+
+            for i in range(num_peripherals):
+                compart = input("Enter volume (L), transition rate (), name of compartment (all seperated by spaces - eg: 5 25 upper-lung)")
+                compart_list = compart.split()
+                compartments.append(compart_list)
+    
+    elif model_type == "sc":
+
+        sub_compart = input("Enter volume (L), transition rate () for the sub compartment (all seperated by spaces - eg: 5 25 )")
+        sub_compart_split = sub_compart.split()
+        sub_compart_split.append(str("Sub"))
+        compartments.append(sub_compart_split)
+
+        main_compart = input("Enter volume (L), transition rate () for the main compartment (all seperated by spaces - eg: 5 25 )")
+        main_compart_split = main_compart.split()
+        main_compart_split.append(str("Main"))
+        compartments.append(main_compart_split)
+
+        num_peripherals = input("How many peripheral compartments do you want to test?")
+        num_peripherals = int(num_peripherals)
+
+        if num_peripherals > 0:
+
+            for i in range(num_peripherals):
+                compart = input("Enter volume (L), transition rate (), name of compartment (all seperated by spaces - eg: 5 25 upper-lung)")
+                compart_list = compart.split()
+                compartments.append(compart_list)
+
 
     vis = input("Would you like to generate a graph? (Y/N)")
 
     print("Thank you! Building model, please wait...")
 
+
     return {
-        'len_model_type': len_model_type, 
         'model_type': model_type, 
         'compound': compound, 
         'dose':dose, 
         'len_assay':len_assay, 
         'len_interval':len_interval, 
         'clearance':clearance, 
-        'len_compartments':len_compartments, 
         'compartments':compartments, 
         'vis':vis
         }
