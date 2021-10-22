a845d6cfada1d41314f2795ed92b158de0b7ae3f
Cameron Anderson <cagfa1@users.noreply.github.com>
2021-10-22 11:28:20 +0100

    additions to visualiser test

--- tests/test_visualiser.py
+++ tests/test_visualiser.py
@@ -9,3 +9,9 @@ def test_single_plot_data():
     assert len(single_plot_data(json_file, csv_file)) == 4
     assert type(single_plot_data(json_file, csv_file)[0]) == 'dict'
     assert type(single_plot_data(json_file, csv_file)[1]) == 'numpy.ndarray'
+
+def test_multiplot():
+    from visualiser import multiplot
+
+def test_makeplots():
+    #Can we add a mock here to see what the function is calling Sam?
