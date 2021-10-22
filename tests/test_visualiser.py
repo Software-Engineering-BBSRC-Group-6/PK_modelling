a10b8fc0b091f1127ca85b66073c8c10f7d72831
Cameron Anderson <cagfa1@users.noreply.github.com>
2021-10-22 11:58:46 +0100

    new test files and updated vis test

--- tests/test_visualiser.py
+++ tests/test_visualiser.py
@@ -3,12 +3,18 @@ print('Running tests on visualiser script')
 def test_single_plot_data():
     from visualiser import single_plot_data
 
-    json_file = './test_json.json'
-    csv_file = './test_csv.csv'
+    json_file = './test_1.json'
+    csv_file = './test_1.csv'
 
     assert len(single_plot_data(json_file, csv_file)) == 4
     assert type(single_plot_data(json_file, csv_file)[0]) == 'dict'
     assert type(single_plot_data(json_file, csv_file)[1]) == 'numpy.ndarray'
+
+def test_collate_data():
+    from visualiser import collate_data
+    filesnames = [['test_1.json', 'test_1.csv'], ['test_2.json', 'test_2.csv']]
+
+    assert len(collate_data(filesnames)) == len(filesnames)
 
 def test_multiplot():
     from visualiser import multiplot
