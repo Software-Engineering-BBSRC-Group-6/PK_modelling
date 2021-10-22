de873a7bca689e673e8869e54326c9908e5f786f
FusionLocus <joejpollacco@gmail.com>
2021-10-22 09:59:54 +0100

    Add basic pip install capability, reorganised directory structure
    Pip install capability installs from requirements file.

--- setup.py
+++ setup.py
@@ -1,3 +1,14 @@
+import pathlib
 import setuptools
+from pkg_resources import parse_requirements
 
-setuptools.setup()
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in parse_requirements(requirements_txt)
+    ]
+
+setuptools.setup(
+    install_requires=install_requires,
+)
