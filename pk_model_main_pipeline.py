533d082e26de9408125634482bedb8b23464098a
danobohud <danobohud@gmail.com>
2021-10-22 13:33:03 +0100

    README updated with workflow

--- pk_model_main_pipeline.py
+++ pk_model_main_pipeline.py
@@ -13,7 +13,7 @@ filenames = []
 for i in range(N):
     # Get user input to build a model.
     current_json = param_to_file()
-    # current_json = 'jsons/1634833362.6048021.json'
+    # current_json = 'jsons/1634901971.5175.json'
 
     # Transform that user input into a dosing protocol function.
     dosing_function = build_dose(current_json)
